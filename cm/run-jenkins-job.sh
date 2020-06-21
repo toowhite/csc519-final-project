84f3a24c35287c4b6c7f405351b797e714381d56
Zhuowei Li <zli58@ncsu.edu>
2020-06-21 14:24:42 -0400

    Change pipeline name to be consistent with requirement

--- cm/run-jenkins-job.sh
+++ cm/run-jenkins-job.sh
@@ -1,4 +1,4 @@
 # echo "I want to run job $1" 
-jenkins-jobs --user admin --password admin update jobs/pipeline.yml
+jenkins-jobs --user admin --password admin update /bakerx/cm/jobs/pipeline.yml
 
 # TODO: trigger job run
