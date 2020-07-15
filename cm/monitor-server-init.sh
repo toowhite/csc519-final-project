63a8a85179843915e41666e4634165f8311dda22
Zhuowei Li <zli58@ncsu.edu>
2020-07-15 17:53:22 -0400

    Init ansible on monitor

---
+++ cm/monitor-server-init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Trace commands as we run them:
+set -x
+
+# Script used to initialize your ansible server after provisioning.
+sudo add-apt-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get upgrade --assume-yes
+sudo apt-get install ansible -y
+
+# Ensure security key has proper permissions
+chmod 700 ~/.ssh/id_rsa
+chmod +x /root/cm/*.sh
+
+set +x
+ansible-playbook /root/cm/monitor-setup.yml
+set -x
--- commands/monitor_setup.js
+++ commands/monitor_setup.js
@@ -6,6 +6,8 @@ const air = require('ansible-inventory-reader');
 const scpSync = require('../lib/scp');
 const sshSync = require('../lib/ssh');
 
+let identifyFile = path.join(os.homedir(), '.ssh', 'id_rsa');
+
 exports.command = 'monitor-setup';
 exports.desc = 'Setup monitoring infrastructure on given infrastructure';
 exports.builder = yargs => {
@@ -38,17 +40,20 @@ function setupMonitor(config) {
     let monitorIp = config.monitor.children[0].host;
 
     console.log(chalk.blueBright('Installing privateKey on jenkins server'));
-    let identifyFile = path.join(os.homedir(), '.ssh', 'id_rsa');
-    result = scpSync (identifyFile, `root@${monitorIp}:/root/.ssh/id_rsa`);
+    result = scpSync (identifyFile, `root@${monitorIp}:/root/.ssh/id_rsa`, identifyFile);
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Copying over vault password file to jenkins server'));
     let vaultPasswordFile = path.join(os.homedir(), '.ansible', '.vault-pass');
-    result = scpSync (vaultPasswordFile, `root@${monitorIp}:/root/.vault-pass`);
+    result = scpSync (vaultPasswordFile, `root@${monitorIp}:/root/.vault-pass`, identifyFile);
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     console.log(chalk.blueBright('Copying init scripts...'));
-    result = scpSync("./cm", `root@${monitorIp}:/root/cm`, identifyFile, true);
+    result = scpSync("./cm", `root@${monitorIp}:/root`, identifyFile, true);
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Running init script...'));
+    result = sshSync(`/root/cm/monitor-server-init.sh`, `root@${monitorIp}`, identifyFile);
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 }
 
