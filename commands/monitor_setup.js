a0e07c15f3527c98c83d9c22b601042bd096fd89
Zhuowei Li <zli58@ncsu.edu>
2020-07-15 17:22:29 -0400

    Sync files to monitor

--- commands/monitor_setup.js
+++ commands/monitor_setup.js
@@ -1,6 +1,10 @@
 const chalk = require('chalk');
-const fs = require('fs');
-const { alias } = require('yargs');
+const path = require('path');
+const os = require('os');
+const air = require('ansible-inventory-reader');
+
+const scpSync = require('../lib/scp');
+const sshSync = require('../lib/ssh');
 
 exports.command = 'monitor-setup';
 exports.desc = 'Setup monitoring infrastructure on given infrastructure';
@@ -24,5 +28,30 @@ exports.handler = async (argv) => {
 };
 
 async function run(inventoryFile) {
-    console.log(inventoryFile);
+    
+    let config = air(inventoryFile);
+    setupMonitor(config);
+    setupAgents(config);
 }
+
+function setupMonitor(config) {
+    let monitorIp = config.monitor.children[0].host;
+
+    console.log(chalk.blueBright('Installing privateKey on jenkins server'));
+    let identifyFile = path.join(os.homedir(), '.ssh', 'id_rsa');
+    result = scpSync (identifyFile, `root@${monitorIp}:/root/.ssh/id_rsa`);
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Copying over vault password file to jenkins server'));
+    let vaultPasswordFile = path.join(os.homedir(), '.ansible', '.vault-pass');
+    result = scpSync (vaultPasswordFile, `root@${monitorIp}:/root/.vault-pass`);
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Copying init scripts...'));
+    result = scpSync("./cm", `root@${monitorIp}:/root/cm`, identifyFile, true);
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+}
+
+function setupAgents(config) {
+    
+} 
