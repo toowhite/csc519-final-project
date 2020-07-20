2180adfde07df6256b6dba3d1ccf561438d5f4d1
Andrew Miller <andrew_miller@bcbsil.com>
2020-07-20 17:29:51 -0500

    revert back to separate deploy scripts

--- commands/deploy.js
+++ commands/deploy.js
@@ -14,7 +14,6 @@ exports.builder = yargs => {
         server: {
             describe: 'name of server to deploy',
             type: 'string',
-            // demandOption: true
         },
         inventory: {
             describe: 'Prod resources config file',
@@ -27,7 +26,7 @@ exports.builder = yargs => {
 
 
 exports.handler = async argv => {
-    const { server, inventory } = argv;
+    const {server, inventory} = argv;
 
     await (async () => {
 
@@ -38,16 +37,26 @@ exports.handler = async argv => {
 };
 
 async function run(server, inventory) {
-    let config = await fs.readFileSync(`cm/${inventory}`, {encoding:'utf8', flag:'r'});
+    let config = await fs.readFileSync(`cm/${inventory}`, {encoding: 'utf8', flag: 'r'});
     let cloudResources = config.split("\n\n");
+
+    let serverConfig;
+    if (server === "checkbox.io")
+        serverConfig = "checkbox";
+    else if (server === "iTrust")
+        serverConfig = "itrust";
+    else {
+        console.log("\n invalid server name.. \n");
+        process.exit(1);
+    }
 
     let ipAddress;
     let ansiblePrivateKeyPath;
     let ansibleUser;
     for (i in cloudResources) {
         let cloudResource = cloudResources[i];
-        if (cloudResource.includes(server)) {
-            let resources = cloudResource.replace( /\n/g, " " ).split( " " );
+        if (cloudResource.includes(serverConfig)) {
+            let resources = cloudResource.replace(/\n/g, " ").split(" ");
             ipAddress = resources[1].trim();
             ansiblePrivateKeyPath = resources[2].trim().replace("ansible_ssh_private_key_file=", "");
             ansibleUser = resources[3].trim().replace("ansible_user=", "");
@@ -61,14 +70,14 @@ async function run(server, inventory) {
 
     console.log(chalk.blueBright(`Installing ansiblePrivateKey on ${server} server`));
     let identifyFile = ansiblePrivateKeyPath || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
-    result = scpSync (identifyFile, `${ansibleUser}@${ipAddress}:/home/vagrant/.ssh/mm_rsa`);
-    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    result = scpSync(identifyFile, `${ansibleUser}@${ipAddress}:/home/vagrant/.ssh/mm_rsa`);
+    if (result.error) {console.log(result.error);process.exit(result.status);}
 
     console.log(chalk.blueBright(`Ensure appropriate permissions on ssh key for ${server}...`));
     result = sshSync(`chmod 700 /home/vagrant/.ssh/mm_rsa`, `${ansibleUser}@${ipAddress}`);
-    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    if (result.error) {console.log(result.error);process.exit(result.status);}
 
     console.log(chalk.blueBright(`Running deploy playbook on config server for ${server}...`));
     result = sshSync(`/bakerx/cm/${server}-deploy.sh /bakerx/cm/${inventory}`, `${ansibleUser}@192.168.33.20`);
-    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+    if (result.error) {console.log(result.error);process.exit(result.status);}
 }
