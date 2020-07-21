54dc0ce12069a4e0c744f6247163b8ad39735fd1
Andrew Miller <andrew_miller@bcbsil.com>
2020-07-20 20:52:58 -0500

    checkbox.io deployment fully automated for local environment

--- cm/checkbox.io-deploy.sh
+++ cm/checkbox.io-deploy.sh
@@ -10,7 +10,15 @@ set +x
 ansible-playbook /bakerx/cm/checkbox.io-deploy.yml --vault-password-file=/home/vagrant/.vault-pass -i $1
 set -x
 
-export GIT_SSH_COMMAND="ssh -i ~/.ssh/mm_rsa -o StrictHostKeyChecking=no"
-#TODO update for possibility of remote host
-git remote add checkbox ssh://vagrant@192.168.33.23/home/vagrant/checkbox/.git
-git push checkbox master
+if [ ! -d "/home/vagrant/check" ]; then
+    mkdir check
+    cd check
+    git clone https://github.com/chrisparnin/checkbox.io.git
+    cd checkbox.io
+    export GIT_SSH_COMMAND="ssh -i ~/.ssh/mm_rsa -o StrictHostKeyChecking=no"
+    #TODO update for possibility of remote host
+    git remote add checkbox ssh://vagrant@192.168.33.23/home/vagrant/checkbox/.git
+    git push checkbox master
+fi
+
+ansible-playbook /bakerx/cm/nginx.yml --vault-password-file=/home/vagrant/.vault-pass -i $1
