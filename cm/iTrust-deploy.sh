539b0615ebf5acfceba6af962344ea4291b8e874
qwang20 <qwang20@ncsu.edu>
2020-07-21 20:54:05 -0400

    deploy itrust install tomcat

--- cm/iTrust-deploy.sh
+++ cm/iTrust-deploy.sh
@@ -6,8 +6,7 @@ set -e
 # Trace commands as we run them:
 set -x
 
-INVENTORY=$1
-
 set +x
 ansible-playbook /bakerx/cm/iTrust-deploy.yml --vault-password-file=/home/vagrant/.vault-pass -i $1
 set -x
+
