fa94a1d3247bae5439256f79b9147cc7a03b5a13
Zhuowei Li <zli58@ncsu.edu>
2020-07-23 13:54:39 -0400

    Fix bugs && minor changes

--- commands/canary.js
+++ commands/canary.js
@@ -5,39 +5,31 @@ const path = require('path');
 const scpSync = require('../lib/scp');
 const sshSync = require('../lib/ssh');
 
-exports.command = 'canary <master> <broken>';
+exports.command = 'canary master broken';
 exports.desc = 'Construct canary infrastructure, collect data, and perform analysis on the given branches';
 exports.builder = yargs => {
     yargs.options({
-        master: {
-            type: 'string',
-            demandOption: true
-		},
-		broken: {
-            type: 'string',
-            demandOption: true
-		}
     });
 };
 
 
 exports.handler = async (argv) => {
-    const {master, broken} = argv;
+    const {} = argv;
     (async () => {
-        await run(master, broken);
+        await run();
     })();
 };
 
-let MASTER_IP = "192.168.33.30";
-let BROKEN_IP = "192.168.33.31";
-let PROXY_IP = "192.168.33.32";
+const MASTER_IP = "192.168.33.30";
+const BROKEN_IP = "192.168.33.31";
+const PROXY_IP = "192.168.33.32";
 
-async function run(master, broken) {
+async function run() {
     console.log(chalk.blueBright('Provisioning needed servers...'));
-    let result = child.spawnSync(`bakerx`, `run ${master} bionic --ip ${MASTER_IP} --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    let result = child.spawnSync(`bakerx`, `run master bionic --ip ${MASTER_IP} --sync`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
-    result = child.spawnSync(`bakerx`, `run ${broken} bionic --ip ${BROKEN_IP} --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    result = child.spawnSync(`bakerx`, `run broken bionic --ip ${BROKEN_IP} --sync`.split(' '), {shell:true, stdio: 'inherit'} );
     if( result.error ) { console.log(result.error); process.exit( result.status ); }
 
     result = child.spawnSync(`bakerx`, `run proxy bionic --ip ${PROXY_IP} --sync`.split(' '), {shell:true, stdio: 'inherit'} );
@@ -50,7 +42,7 @@ async function run(master, broken) {
     console.log(chalk.redBright('Be patient. It takes more than 2 minutes to generate the report.'));
     setTimeout(() => {
         console.log(chalk.blueBright('Try getting test result...'));
-        result = sshSync(`sudo cat /dashboard/result.txt`, `vagrant@${PROXY_IP}`);
+        result = sshSync(`sudo cat /bakerx/Monitoring/dashboard/result.txt`, `vagrant@${PROXY_IP}`);
         if (result.error) { console.log(result.error); process.exit(result.status); }
     
     }, (60 + 60 + 5) * 1000);
